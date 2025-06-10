66b4f3b51c6ba2d96cadd5d6878ad698f187b376
Slava <31252907+adainx@users.noreply.github.com>
2025-06-10 17:03:41 +0300

    Portfolio and CV Update
    - Added new artwork
    - Updated CV
    - Updated .JS and .HTML file (now it is possible to make more categories for one artwork)

--- 2d_portfolio/script.js
+++ 2d_portfolio/script.js
@@ -9,109 +9,114 @@ document.addEventListener('DOMContentLoaded', function() {
     // Sample images data with relative paths
     const images = [
 	{ 
+            src: 'images/thumbnails/75_thumbnail.jpg', 
+            fullSrc: 'images/75.jpg', 
+            category: ['freelance', 'char_design'] // Массив категорий
+        },
+        { 
             src: 'images/thumbnails/fr_image_14_thumbnail.jpg', 
             fullSrc: 'images/73.jpg', 
-            category: 'freelance' 
+            category: ['freelance']
         },
-	{ 
+        { 
             src: 'images/thumbnails/test_HoR_1.jpg', 
             fullSrc: 'images/fr_image_13.jpg', 
-            category: 'freelance' 
+            category: ['freelance', 'char_design']
         },
-	{ 
+        { 
             src: 'images/thumbnails/test_HoR_2.jpg', 
             fullSrc: 'images/fr_image_12.jpg', 
-            category: 'freelance' 
+            category: ['freelance', 'char_design']
         },
-	{ 
+        { 
             src: 'images/thumbnails/63_thumbnail.jpg', 
             fullSrc: 'images/63.jpg', 
-            category: 'freelance' 
+            category: ['freelance', 'char_design']
         },
         { 
             src: 'images/thumbnails/fr_image11-thumbnail.jpg', 
             fullSrc: 'images/fr_image11.jpg', 
-            category: 'freelance' 
+            category: ['freelance', 'char_design']
         },
         { 
             src: 'images/thumbnails/fr_image10-thumbnail.jpg', 
             fullSrc: 'images/fr_image10.jpg', 
-            category: 'freelance' 
+            category: ['freelance', 'char_design']
         },
         { 
             src: 'images/thumbnails/test_HoR_3.jpg', 
             fullSrc: 'images/fr_image8_1.jpg', 
-            category: 'freelance' 
+            category: ['freelance', 'char_design']
         },
         { 
             src: 'images/thumbnails/test_HoR_4.jpg', 
             fullSrc: 'images/fr_image9.jpg', 
-            category: 'freelance' 
+            category: ['freelance', 'char_design']
         },
         { 
             src: 'images/thumbnails/fr_image7-thumbnail.jpg', 
             fullSrc: 'images/fr_image7.jpg', 
-            category: 'freelance' 
+            category: ['freelance', 'char_design']
         },
         { 
             src: 'images/thumbnails/image7-thumbnail.jpg', 
             fullSrc: 'images/image7.jpg', 
-            category: 'personal' 
+            category: ['personal']
         },
         { 
             src: 'images/thumbnails/image100-thumbnail.jpg', 
             fullSrc: 'images/image100.jpg', 
-            category: 'personal' 
+            category: ['personal']
         },
         { 
             src: 'images/thumbnails/image99-thumbnail.jpg', 
             fullSrc: 'images/image99.jpg', 
-            category: 'personal' 
+            category: ['personal']
         },
-	{ 
+        { 
             src: 'images/thumbnails/image10-thumbnail.jpg', 
             fullSrc: 'images/image10.jpg', 
-            category: 'personal' 
+            category: ['personal']
         },
-	{ 
+        { 
             src: 'images/thumbnails/image9-thumbnail.jpg', 
             fullSrc: 'images/image9.jpg', 
-            category: 'personal' 
+            category: ['personal']
         },
         { 
             src: 'images/thumbnails/image8-thumbnail.jpg', 
             fullSrc: 'images/image8.jpg', 
-            category: 'personal' 
+            category: ['personal']
         },
         { 
             src: 'images/thumbnails/image6-thumbnail.jpg', 
             fullSrc: 'images/image6.jpg', 
-            category: 'personal' 
+            category: ['personal']
         },
         { 
             src: 'images/thumbnails/image5-thumbnail.jpg', 
             fullSrc: 'images/image5.jpg', 
-            category: 'personal' 
+            category: ['personal']
         },
         { 
             src: 'images/thumbnails/image4-thumbnail.jpg', 
             fullSrc: 'images/image4.jpg', 
-            category: 'personal' 
+            category: ['personal']
         },
         { 
             src: 'images/thumbnails/image3-thumbnail.jpg', 
             fullSrc: 'images/image3.jpg', 
-            category: 'personal' 
+            category: ['personal']
         },
         { 
             src: 'images/thumbnails/image2-thumbnail.jpg', 
             fullSrc: 'images/image2.jpg', 
-            category: 'personal' 
+            category: ['personal']
         },
         { 
             src: 'images/thumbnails/image1-thumbnail.jpg', 
             fullSrc: 'images/image1.jpg', 
-            category: 'personal' 
+            category: ['personal']
         }
         // Add more images with respective thumbnails and fullSrc paths
     ];
@@ -119,11 +124,13 @@ document.addEventListener('DOMContentLoaded', function() {
     // Function to display images
     function displayImages(filter) {
         gallery.innerHTML = '';
-        const filteredImages = filter === 'all' ? images : images.filter(img => img.category === filter);
+        const filteredImages = filter === 'all' ? images : images.filter(img => 
+            Array.isArray(img.category) ? img.category.includes(filter) : img.category === filter
+        );
         filteredImages.forEach(img => {
             const imgElement = document.createElement('img');
             imgElement.src = img.src;
-            imgElement.alt = img.category;
+            imgElement.alt = Array.isArray(img.category) ? img.category.join(', ') : img.category;
             imgElement.addEventListener('click', () => {
                 openFullImage(img.fullSrc);
             });
